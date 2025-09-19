2eafa3668cbd2397bf4d2f4377ea7013ed53f1f4
Oliver Long <longoliver2005@gmail.com>
2025-09-19 17:57:30 -0400

    Create Scenes

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+
+    public float myVar;
+    public GameObject myObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class TestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector2 currentPos = transform.position;
+        transform.position += Vector3.left * Time.deltaTime * myVar;
+        //gameObject.SetActive()
     }
+
+    
 }
