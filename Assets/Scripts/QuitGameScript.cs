e32a76b929769cefd29654b40d71bd57159dc919
Oliver Long <longoliver2005@gmail.com>
2025-09-21 16:44:01 -0400

    Ending Screen

---
+++ Assets/Scripts/QuitGameScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space)) {
+            QuitGame();
+        }
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting."); // Optional: for debugging purposes
+    }
+}
